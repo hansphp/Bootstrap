0f512c7b486f489e7b53275366fada682b70d315
eHans <hans.php@gmail.com>
2014-08-14 00:23:10 -0500

    Carga inicial
    Propuesta de temas para UserMyAdmin

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">UserMyAdmin</h3>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="#">Ingreso al sistema</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Ayuda</a></li>
+              </ul>
+            </div>
+          </div>
+
+        <!--  <div class="inner cover">
+            <h1 class="cover-heading">Cover your page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+            </p>
+          </div>-->
+          
+           <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Ingreso al sistema</h2>
+        <input type="email" class="form-control" placeholder="Correo electrónico" required autofocus>
+        <input type="password" class="form-control" placeholder="Contraseña" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Recordarme
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesión</button>
+      </form>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p><a href="http://edificapps.com/usermyadmin">UserMyAdmin</a> by <a href="http://edificapps.com">EdificApps</a>.</p><p>Reengineering business</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
